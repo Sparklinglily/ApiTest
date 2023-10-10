3fceb24afb9f9faab4dd5eeb806eaa9813d42209
Sparklinglily <carlaejemeh@gmail.com>
2023-10-10 13:54:00 +0100

    jsonPlaceholder test

--- lib/apiConfig.dart
+++ lib/apiConfig.dart
@@ -1,1 +1,20 @@
+import 'package:apitest/models/album.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+// Future<http.Response> fetchAlbum() async {
+// return http.get(Uri.parse('https: jsonplaceholder.typicode.com/albums/1'));
+// }
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('failed to load album');
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,45 @@
+import 'package:apitest/apiConfig.dart';
+import 'package:apitest/models/album.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //late keyword means that a non-nullable
+  // keyword will be initialised later in the code.
+
+  //fetch data
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(
+                  snapshot.data!.title,
+                  style: TextStyle(fontSize: 30),
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return CircularProgressIndicator();
+            }),
+      ),
+    );
   }
 }
