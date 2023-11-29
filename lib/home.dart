cb2e8da089846ea3de6f226a68ef10a393e7fc02
Sparklinglily <carlaejemeh@gmail.com>
2023-11-29 21:22:56 +0100

    appbar fix

--- lib/home.dart
+++ lib/home.dart
@@ -29,9 +29,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('Json placholder Test API')),
-        ),
+        appBar: AppBar(),
         body: userModel == null || userModel!.isEmpty
             ? const Center(child: CircularProgressIndicator())
             : ListView.builder(
